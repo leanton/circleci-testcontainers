9de2bec28f0480bfc341b6375766fe41acefdac9
Anton Le <mail@antonle.me>
2021-11-06 22:21:03 +0000

    testcontainers PG example

---
+++ src/test/java/me/antonle/testcontainer/ContainerTest.java
@@ -0,0 +1,23 @@
+package me.antonle.testcontainer;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContainerTest extends PgSupport {
+
+    @Test
+    void should_connect_to_database() {
+        assertTrue(PG.isCreated());
+        assertTrue(PG.isRunning());
+
+        try (var statement = PG_CLIENT.createStatement()) {
+            var execute = statement.execute("SELECT 1");
+            assertTrue(execute);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/me/antonle/testcontainer/PgSupport.java
@@ -0,0 +1,33 @@
+package me.antonle.testcontainer;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PgSupport {
+
+    protected static final PostgreSQLContainer<?> PG;
+    protected static final Connection PG_CLIENT;
+
+    public static final String DB_URL;
+    public static final String DB_NAME = "db";
+    private static final String DB_USER = "test";
+
+    static {
+        PG = new PostgreSQLContainer<>("postgres:9.6.12")
+            .withDatabaseName(DB_NAME)
+            .withUsername(DB_USER)
+            .withPassword(DB_USER);
+        PG.start();
+        DB_URL = PG.getJdbcUrl();
+
+        try {
+            PG_CLIENT = DriverManager.getConnection(DB_URL, DB_USER, DB_USER);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
